2a29582ce563d2d91a100afed779bfe347942498
cdu <>
2024-03-15 17:34:29 +0300

    add pseudo icon for password items

--- ui/memo-list/src/main/java/midget17468/memo/compose/composable/MemoIcon.kt
+++ ui/memo-list/src/main/java/midget17468/memo/compose/composable/MemoIcon.kt
@@ -1,10 +1,18 @@
 package midget17468.memo.compose.composable
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+import midget17468.memo.memo_list.R
 import midget17468.memo.model.domain.MemoType
 
 @Composable
@@ -15,7 +23,22 @@ fun MemoIcon(
     Surface(
         shape = RoundedCornerShape(percent = 50),
         color = MaterialTheme.colorScheme.primary,
-        content = { },
+        content = {
+            Box(
+                contentAlignment = Alignment.Center
+            ) {
+                Text(
+                    text = stringResource(
+                        id = when (type) {
+                            MemoType.Password -> R.string.memoItem_icon_text_password
+                        }
+                    ),
+                    fontFamily = FontFamily.Monospace,
+                    fontSize = 12.sp,
+                    fontWeight = FontWeight.Black,
+                )
+            }
+        },
         modifier = modifier,
     )
 }
