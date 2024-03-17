17b7a50576ca4050c749de7ee892eac0d1d4aad6
cdu <>
2024-03-17 17:05:26 +0300

    change item card colors

--- ui/memo-list/src/main/java/midget17468/memo/compose/composable/MemoItem.kt
+++ ui/memo-list/src/main/java/midget17468/memo/compose/composable/MemoItem.kt
@@ -10,10 +10,10 @@ import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
+import androidx.compose.material3.surfaceColorAtElevation
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import midget17468.compose.halfMargin
@@ -26,11 +26,13 @@ internal fun MemoItem(
     modifier: Modifier = Modifier
 ) {
     val containerColor =
-        if (item.isExpanded) {
-            Color.Unspecified
-        } else {
-            MaterialTheme.colorScheme.background
-        }
+        MaterialTheme.colorScheme.surfaceColorAtElevation(
+            if (item.isExpanded) {
+                8.dp
+            } else {
+                2.dp
+            }
+        )
     Card(
         colors = CardDefaults.cardColors(containerColor),
     ) {
