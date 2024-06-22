674f5bf995775bbc89f992ac1a1aee232b2f9fa0
cdu <>
2024-06-22 14:14:10 +0300

    make 'i forgot' button outlined in RepetitionScreen

--- feature/repetition/src/main/java/cdu278/repetition/ui/composable/CheckingButtons.kt
+++ feature/repetition/src/main/java/cdu278/repetition/ui/composable/CheckingButtons.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Button
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -52,7 +53,7 @@ internal fun CheckingButtons(
         }
         when (state.mode) {
             Repetition ->
-                Button(
+                OutlinedButton(
                     onClick = forget,
                     modifier = Modifier
                         .width(buttonWidth)
