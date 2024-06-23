4cf4028870096ddfca9773a8005e66e433d76511
cdu <>
2024-06-23 21:18:18 +0300

    add password and pin icons

--- feature/repetition-list/src/main/java/cdu278/repetition/ui/composable/RepetitionIcon.kt
+++ feature/repetition-list/src/main/java/cdu278/repetition/ui/composable/RepetitionIcon.kt
@@ -2,20 +2,17 @@ package cdu278.repetition.ui.composable
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.sp
-import cdu278.intervals.repetition.list.ui.R
+import androidx.compose.ui.res.painterResource
 import cdu278.repetition.RepetitionType
 import cdu278.repetition.RepetitionType.Password
 import cdu278.repetition.RepetitionType.Pin
+import cdu278.foundation.android.R as CommonR
 
 @Composable
 fun RepetitionIcon(
@@ -30,16 +27,14 @@ fun RepetitionIcon(
         Box(
             contentAlignment = Alignment.Center,
         ) {
-            Text(
-                text = stringResource(
+            Icon(
+                painterResource(
                     id = when (type) {
-                        Password -> R.string.repetitionItem_icon_text_password
-                        Pin -> R.string.repetitionItem_icon_text_pin
+                        Password -> CommonR.drawable.ic_password
+                        Pin -> CommonR.drawable.ic_pin
                     }
                 ),
-                fontFamily = FontFamily.Monospace,
-                fontSize = 12.sp,
-                fontWeight = FontWeight.Black,
+                contentDescription = null
             )
         }
     }
